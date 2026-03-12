9b33b8b7dea7665695f5d2de3f3ea2a3cff833e1
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-12 13:58:54 +0600

    Prop Types And how to pass/read a prop

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,58 @@ function App() {
       <h1>React Cire Concepts</h1>
       <Student></Student>
       <Student></Student>
-      <Person name="shafrin Ahamed" age = "27"></Person>
-      <Developer name="shakib" tech = "react"></Developer>
-      <Developer name="babu" tech = "JS"></Developer>
+      <Person name="shafrin Ahamed" age="27"></Person>
+      <Developer name="shakib" tech="react"></Developer>
+      <Developer name="babu" tech="JS"></Developer>
+      <Device deviceName="Laptop" ram="8GB"></Device>
+      <Player name="shakib" runs="10,000"></Player>
+      <Player name="Mushi" runs="5,000"></Player>
+      <Salami event="Rojar Eid" amount = "500"></Salami>
+      <Salami event="Graduation" amount = "10000"></Salami>
+      <Books bookName = "Physics" division = "Science"></Books>
     </>
+  );
+}
+
+function Books({bookName, division}){
+  return (
+    <div className="student">
+      <p>Books Name: {bookName} </p>
+      <p>Class: {division}</p>
+    </div>
+  )
+}
+
+function Salami({event, amount}){
+  return (
+    <div className="student">
+      <p>Salami For: {event}</p>
+      <p>Amount: {amount} TK</p>
+
+    </div>
+  );
+}
+
+// const {name, runs} = {name: "tamim", runs: "10,000"};
+
+function Player({name, runs}) {
+  // console.log(props);
+  return (
+    <div className="student">
+      <h3>Player Name: {name} </h3>
+      <p>Runs: {runs} </p>
+    </div>
+  );
+}
+
+const { name, No } = { name: "Shakib Al Hasan", No: 75 };
+
+function Device(props) {
+  return (
+    <div>
+      <p>Device Name: {props.deviceName} </p>
+      <p>RAM: {props.ram} </p>
+    </div>
   );
 }
 
@@ -46,10 +94,10 @@ function Person(props) {
     color: "red",
     textAlign: "center",
   };
-  
+
   return (
     <p style={personStyle}>
-      I'm {props.name} , and I'm {props.age}  years old
+      I'm {props.name} , and I'm {props.age} years old
     </p>
   );
 }
